3b8c78b0eceaab786cc5a52f57cdc2a1f333ddc3
Cloud <kevia@triangle.host>
2017-04-27 01:11:15 -0700

    Configuring webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+
+  entry: [
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+      './main.js'
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: { presets: ['es2015', 'react'] }
+      }]
+  },
+
+  resolve: {
+      extensions: ['.js']
+  },
+
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      filename: 'bundle.js'
+  },
+
+  devServer: {
+      contentBase: './dist',
+      hot: true
+  },
+
+  plugins: [
+      new webpack.HotModuleReplacementPlugin()
+  ]
+
+};
