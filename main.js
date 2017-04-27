8c77bd5884a79a3a3ac40893265fffce9030e3bc
Cloud <kevia@triangle.host>
2017-04-27 02:07:06 -0700

    Hot loading is cool

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ const ReactDOM = require('react-dom');
 
 class TweetBox extends React.Component {
 
+  handleChange(event) {
+    console.log('yup');
+  }
+
   render() {
 
     return (
         <div className="well clearfix">
-          <textarea className="form-control"></textarea>
+          <textarea className="form-control" onChange={this.handleChange.bind(this)}></textarea>
           <br/>
-          <button className="btn btn-primary pull-right">Tweet</button>
+          <button className="btn btn-primary pull-right" disabled>Tweet</button>
         </div>
     );
   }
